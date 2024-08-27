31b97e7165d6a4c10cf2c5d0721908fd9a3349d3
John Kottis <johnkottis@gmail.com>
2024-08-27 15:06:52 +0100

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,14 +1,16 @@
+// routes/clientRoutes.js
+
 const express = require('express');
-const { addClient, getClients } = require('../models/clientModel');
+const { getClients, addClient } = require('../models/clientModel');
 
 const router = express.Router();
 
-// GET /api/clients
+// GET /api/clients - Return all clients as JSON
 router.get('/', (req, res) => {
   res.json(getClients());
 });
 
-// POST /api/clients
+// POST /api/clients - Add a new client and return it as JSON
 router.post('/', (req, res) => {
   const newClient = addClient(req.body);
   res.status(201).json(newClient);
