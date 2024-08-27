10a799ef1ee482efe9e947de3a72cddcd99d4d43
John Kottis <johnkottis@gmail.com>
2024-08-27 15:06:40 +0100

    Update colleagueRoutes.js

--- routes/colleagueRoutes.js
+++ routes/colleagueRoutes.js
@@ -1,14 +1,16 @@
+// routes/colleagueRoutes.js
+
 const express = require('express');
-const { addColleague, getColleagues } = require('../models/colleagueModel');
+const { getColleagues, addColleague } = require('../models/colleagueModel');
 
 const router = express.Router();
 
-// GET /api/colleagues
+// GET /api/colleagues - Return all colleagues as JSON
 router.get('/', (req, res) => {
   res.json(getColleagues());
 });
 
-// POST /api/colleagues
+// POST /api/colleagues - Add a new colleague and return it as JSON
 router.post('/', (req, res) => {
   const newColleague = addColleague(req.body);
   res.status(201).json(newColleague);
